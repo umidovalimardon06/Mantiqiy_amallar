0f9fde1c4abb4d4684604718ff18cc129315b295
umidovalimardon06 <umidovalimardon06@gmail.com>
2024-12-11 02:32:37 +0500

    add Boolean06

---
+++ Boolean06/Main/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+// Umidov Alimardon
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int A,B,C;
+        boolean natija;
+
+        System.out.print("A ni kiriting:");
+        A = in.nextInt();
+        System.out.print("B ni kiriting:");
+        B = in.nextInt();
+        System.out.print("C ni kiriting:");
+        C = in.nextInt();
+
+        natija = (A<=B) && (B<=C);
+
+        System.out.println("A<=B<=C ?\n"+natija);
+
+    }
+}
