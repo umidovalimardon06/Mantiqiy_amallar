b7ba59bf2f4f2fd47504edb3174a1043d9c59aaf
umidovalimardon06 <umidovalimardon06@gmail.com>
2024-12-11 09:21:39 +0500

    add Boolean10

---
+++ Boolean10/Main/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+// Umidov Alimardon.
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int A,B;
+        boolean natija ;
+
+        System.out.print("A:");
+        A = in.nextInt();
+        System.out.print("B:");
+        B = in.nextInt();
+
+        natija = ((A%2>0) && (B%2==0))  || ((B%2>0) && (A%2==0))  ;
+
+        System.out.println("A va B sonlaring faqat bittasi toq son?\n"+natija);
+    }
+}
