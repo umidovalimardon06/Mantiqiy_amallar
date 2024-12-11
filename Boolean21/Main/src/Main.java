781edfc4c97e0bb101c58617def169c34595646e
umidovalimardon06 <umidovalimardon06@gmail.com>
2024-12-12 04:48:30 +0500

    add Boolean21

---
+++ Boolean21/Main/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+// Umidov Alimardon
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int num1,num2,num3;
+        boolean natija;
+
+        System.out.print("num1:");
+        num1 = in.nextInt();
+        System.out.print("num2:");
+        num2 = in.nextInt();
+        System.out.print("num3:");
+        num3 = in.nextInt();
+
+        natija = ( (num1<num2) && (num2<num3) );
+
+        System.out.println("Berilgan sonlar ketma-ket o'suchi?\n"+natija);
+    }
+}
