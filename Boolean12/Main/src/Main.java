35d4acff99ff3b7fd321b1fb55e36d2dd9f6a0c0
umidovalimardon06 <umidovalimardon06@gmail.com>
2024-12-11 10:10:40 +0500

    add Boolean12

---
+++ Boolean12/Main/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+// Umidov Alimardon.
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int A,B,C;
+        boolean natija ;
+
+        System.out.print("A:");
+        A = in.nextInt();
+        System.out.print("B:");
+        B = in.nextInt();
+        System.out.print("C:");
+        C = in.nextInt();
+
+        natija = (A>0) && (B>0) && (C>0) ;
+
+        System.out.println("A,B va C sonlaring har biri musbat?\n"+natija);
+    }
+}
